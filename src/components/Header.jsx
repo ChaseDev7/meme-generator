6bf81ca2890c656a12452040a9a84d82b66c027d
ChaseDev7 <chasewebdev101@gmail.com>
2023-12-09 12:00:10 -0800

    Create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 export default function Header() {
   return (
     <div className="header">
-      <img src="../../public/images/TrollFace.png" />
+      <div className="header-logo">
+        <img src="../../public/images/TrollFace.png" className="troll-face" />
+        <h3 className="header-title">Meme Generator</h3>
+      </div>
+      <p className="project-title">React Course - Project 3</p>
     </div>
   )
 }
