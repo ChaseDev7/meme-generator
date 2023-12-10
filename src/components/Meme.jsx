58d17bd338cab0e782fb95c229d8e71076909f33
ChaseDev7 <chasewebdev101@gmail.com>
2023-12-10 15:47:12 -0800

    Add state to meme component

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,17 +1,32 @@
+import memesData from '../../memesData.jsx'
+import { useState } from 'react';
+
 export default function Meme() {
+
+  const [memeImage, getMemeImage] = useState(" ")
+
+  function handleClick() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    getMemeImage(memesArray[randomNumber].url)
+  }
+
   return (
     <div className="form-container">
-      <form className="form">
-        <fieldset className="top-text-container">
+      <div className="form">
+        <div className="top-text-container">
           <label htmlFor="top-text" className="top-text-label">Top Text</label>
           <input id="top-text" type="text" />
-        </fieldset>
-        <fieldset className="bottom-text-container">
+        </div>
+        <div className="bottom-text-container">
           <label htmlFor="bottom-text" className="bottom-text-label">Bottom Text</label>
           <input id="bottom-text" type="text" />
-        </fieldset>
-      </form>
-      <button className="meme-button">Get a new meme image</button>
+        </div>
+      </div>
+      <button onClick={handleClick} className="meme-button">Get a new meme image</button>
+      <div className="meme-image">
+        <img src={memeImage} />
+      </div>
     </div>
   )
 }
