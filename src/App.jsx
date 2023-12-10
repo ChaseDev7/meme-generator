e1ea3fedd54fddc4b21cc772a4a10b6c77e73922
ChaseDev7 <chasewebdev101@gmail.com>
2023-12-09 17:12:21 -0800

    Add form to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 import './index.css'
 import Header from './components/Header'
+import Form from './components/Form'
 
 function App() {
   return (
     <>
       <Header />
+      <Form />
     </>
   )
 }
