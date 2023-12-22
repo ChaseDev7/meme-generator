866821491c5d305cb44b3e2536e420d443a776e8
ChaseDev7 <chasewebdev101@gmail.com>
2023-12-21 20:11:49 -0800

    Connect meme object state with text inputs

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,12 +3,33 @@ import { useState } from 'react';
 
 export default function Meme() {
 
-  const [memeImage, getMemeImage] = useState(" ")
+  const [meme, setmeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData)
 
   function handleClick() {
-    const memesArray = memesData.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    getMemeImage(memesArray[randomNumber].url)
+    const memesArray = allMemeImages.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    setmeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }))
+  }
+
+  function handleChange(event) {
+    const {name, value} = event.target
+
+    setmeme(prevMeme => {
+      return {
+        ...prevMeme,
+        [name]: value
+      }
+    })
   }
 
   return (
@@ -16,16 +37,30 @@ export default function Meme() {
       <div className="form">
         <div className="top-text-container">
           <label htmlFor="top-text" className="top-text-label">Top Text</label>
-          <input id="top-text" type="text" />
+          <input 
+            id="top-text" 
+            type="text"
+            name="topText"
+            onChange={handleChange}
+            value={meme.topText}
+             />
         </div>
         <div className="bottom-text-container">
           <label htmlFor="bottom-text" className="bottom-text-label">Bottom Text</label>
-          <input id="bottom-text" type="text" />
+          <input 
+            id="bottom-text" 
+            type="text"
+            name="bottomText"
+            onChange={handleChange}
+            value={meme.bottomText}
+             />
         </div>
       </div>
       <button onClick={handleClick} className="meme-button">Get a new meme image</button>
-      <div className="meme-image">
-        <img src={memeImage} />
+      <div className="meme-container">
+        <img src={meme.randomImage} className="meme-image" />
+        <h2 className="meme-top-text">{meme.topText}</h2>
+        <h2 className="meme-bottom-text">{meme.bottomText}</h2>
       </div>
     </div>
   )
