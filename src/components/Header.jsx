55aa170ed6ce6a40555967bbe475a7061eef459d
ChaseDev7 <chasewebdev101@gmail.com>
2023-12-26 16:54:33 -0800

    Import image from file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import trollFace from '../images/TrollFace.png'
+
 export default function Header() {
   return (
     <div className="header">
       <div className="header-logo">
-        <img src="../images/TrollFace.png" className="troll-face" />
+        <img src={trollFace} className="troll-face" />
         <h3 className="header-title">Meme Generator</h3>
       </div>
       <p className="project-title">React Course - Project 3</p>
