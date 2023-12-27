63b4642f21f8189700f87920ef151a625ab0efd9
ChaseDev7 <chasewebdev101@gmail.com>
2023-12-26 16:39:13 -0800

    Update file path for image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="header-logo">
-        <img src="../../public/images/TrollFace.png" className="troll-face" />
+        <img src="../images/TrollFace.png" className="troll-face" />
         <h3 className="header-title">Meme Generator</h3>
       </div>
       <p className="project-title">React Course - Project 3</p>
